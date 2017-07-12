e6599d238021bf35b95c2679c7b4bf253328d5cd
Brenton Moodley <bmoodley@student.42.fr>
2017-07-12 20:30:29 +0200

    added verification functions

---
+++ push_swap/ps_2.c
@@ -0,0 +1,46 @@
+int		verify_int(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 0;
+	while(i < argc)
+	{
+		while (j != '\0')
+		{
+			if (!(ft_isdigit(argv[i][j]) && ft_isspace(argv[i][j])))
+			{
+				return (0)
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	return (1);//return from next verification
+}
+
+int		verify_dup(int argc, char **argv)
+{
+	int i;
+	int pos;
+	cha
+
+	i = 1;
+	while (i < argc)
+	{
+		pos = i;
+		while (i < argc - 1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) != 0)
+				i++;
+			else
+				return (0);
+		}
+		i = pos + 1;
+	}
+	return (1);//or what next verifaction returns
+}
+
+int		verify_int()
---
+++ push_swap/push_swap.c
@@ -0,0 +1,11 @@
+int main(int argc, char ** argv)
+{
+	if (argc == 1)
+	{
+		//error message
+		return (1)
+	}
+	verify_args(argc, argv);
+	popu_arr();
+	return (0);
+}
