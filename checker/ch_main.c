351cc2dedb0fe0600a12c8c19883789321bed594
Brenton Moodley <bmoodley@student.42.fr>
2017-07-17 20:19:11 +0200

    created ch_main.c

--- checker/ch_main.c
+++ checker/ch_main.c
@@ -2,22 +2,19 @@
 #include "../libft/includes/libft.h"
 #include <stdio.h>
 
+void	read_instructions()
+{
+	//with the use of GNL
+}
+
 int		main(int argc, char **argv)
 {
 	t_pack	pack;
-	int		i;
-
-	i = 0;
+	
 	if (!(verify_argc(argc)))
 		return (1);
 	if (!(verify_args(argv[1], &pack)))
 		return (1);
-	ft_nhlaka(pack.int_arr, pack.elements);//handoff to Nhlaka
-/*
-**	**prints contents of array**
-**
-**	while (i < pack.elements)
-**		printf("-%d\n", pack.int_arr[i++]);
-*/
+	ft_putstr("winner winner chicken dinner");
 	return (0);
 }
---
+++ checker/comp.sh
@@ -0,0 +1,1 @@
+gcc ch_main.c ../push_swap/verify.c ../push_swap/verify_controller.c -L ../libft/ -lft -I ../libft/includes/
