cd9cec012592d4c79a295cdbe6dabf06ef0db54c
Brenton Moodley <bmoodley@student.42.fr>
2017-07-17 20:01:20 +0200

    commented handoff

--- push_swap/ps_main.c
+++ push_swap/ps_main.c
@@ -11,8 +11,13 @@ int main(int argc, char ** argv)
 	if (!(verify_argc(argc)))
 		return (1);
 	if (!(verify_args(argv[1], &pack)))
-		return (0);
-	while (i < pack.elements)
-		printf("-%d\n", pack.int_arr[i++]);
+		return (1);
+	ft_nhlaka(pack.int_arr, pack.elements);//handoff to Nhlaka
+/*
+**	**prints contents of array**
+**
+**	while (i < pack.elements)
+**		printf("-%d\n", pack.int_arr[i++]);
+*/
 	return (0);
 }
