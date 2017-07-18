8ac226726f6f4bc554a1844b3ea55165f60a7f65
Jerome Damhuis <jerome.damhuis@gmail.com>
2017-07-18 12:40:00 +0200

    getting to my version

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -18,8 +18,10 @@
 # include "get_next_line.h"
 # define TRUE 1
 # define FALSE 0
+# define INT_MIN -2147483648
+# define INT_MAX 2147483647
 
-typedef int t_bool;
+typedef int			t_bool;
 
 typedef struct		s_list
 {
