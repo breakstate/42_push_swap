df21ee64e3083740b96b68c3d3b7afb310b2e14e
Jerome DAMHUIS <jdamhuis@e4r15p10.jnb.42.fr>
2017-07-20 11:32:26 +0200

    making a new file for assaign_weights

--- pathing.c
+++ pathing.c
@@ -60,26 +60,3 @@ void		apply_rule(char *move, t_stack *a, t_stack *b)
 	if (ft_strcmp("RRR", move) == 0)
 		ft_RRR(a, b);
 }
-
-int			assaign_weight(t_node *node)
-{
-	t_list	a_copy;
-	t_list	b_copy;
-	char	**moves_list;
-	size_t	index;
-
-	a_copy = copy_linked_list(node->a);
-	b_copy = copy_linked_list(node->b);
-	moves_list = move_list(node);
-	index = 0;
-	while(index < node->steps)
-	{
-		apply_rule(move[index], a_copy, b_copy);
-		index++;
-	}
-	get_weight(uygkyu);
-	free(move_list k);
-	free(a_copy link list);
-	free(b_copy linklist)
-	return (1);
-}
--- push_swap.h
+++ push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdamhuis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/20 11:12:51 by jdamhuis          #+#    #+#             */
+/*   Updated: 2017/07/20 11:17:03 by jdamhuis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "libft.h"
@@ -7,6 +19,19 @@
 **	structs structure
 **	-----------------/
 */
+
+typedef struct		s_list
+{
+	int				value;
+	struct s_list	*next;
+	struct s_list	*prev;
+}					t_list;
+
+typedef struct		s_stack
+{
+	t_list			*front;
+	t_list			*back;
+}					t_stack;
 
 typedef struct		s_node
 {
@@ -18,43 +43,29 @@ typedef struct		s_node
 	t_stack			b;
 }					t_node;
 
-typedef struct	s_list
-{
-	int			 value;
-	struct s_list	*next;
-	struct s_list	*prev;
-}				t_list;
-
-typedef struct	s_stack
-{
-	t_list		*front;
-	t_list		*back;
-}				t_stack;
-
 /*
 **	-----------------\
 **	struct for int arr
 **	-----------------/
 */
 
-typedef struct	s_pack
+typedef struct		s_pack
 {
-	int			*int_arr;
-	int			elements;
-}				t_pack;
-
+	int				*int_arr;
+	int				elements;
+}					t_pack;
 
 /*
 **	in file ps_verify.c
 */
 
-int				verify_dup(char *arg);
-int				verify_digits(char *arg);
-int				verify_min_max(long num);
-int				verify_int(char **arg);
-int				arr_stoi(char **arg, t_pack *pack);
-int				verify_args(char *arg, t_pack *pack);
-int				verify_argc(int argc);
+int					verify_dup(char *arg);
+int					verify_digits(char *arg);
+int					verify_min_max(long num);
+int					verify_int(char **arg);
+int					arr_stoi(char **arg, t_pack *pack);
+int					verify_args(char *arg, t_pack *pack);
+int					verify_argc(int argc);
 
 /*
 **	-----------------------\
@@ -62,18 +73,28 @@ int				verify_argc(int argc);
 **	-----------------------/
 */
 
-t_list			copy_linked_list(t_list *list);
-void			ft_add_elem_front(t_list **top, int value);
-t_list			*ft_add_elem_back(t_list **back, int value);
-t_stack			*ft_init_stack(int *a, int size);
-void			ft_swap(t_stack *s);
-t_list			*ft_pop_out_back(t_list **back);
-t_list			*ft_pop_out_front(t_list **front);
-void			ft_free_list(t_list **list);
-t_stack			*ft_create_stack(void);
-void			ft_SA_SB(t_stack *a);
-void			ft_SS(t_stack *a, t_stack *b);
-void			ft_PA_PB(t_stack *from, t_stack *to);
-void			ft_free_stack(t_stack *stack);
+t_list				copy_linked_list(t_list *list);
+void				ft_add_elem_front(t_list **top, int value);
+t_list				*ft_add_elem_back(t_list **back, int value);
+t_stack				*ft_init_stack(int *a, int size);
+void				ft_swap(t_stack *s);
+t_list				*ft_pop_out_back(t_list **back);
+t_list				*ft_pop_out_front(t_list **front);
+void				ft_free_list(t_list **list);
+t_stack				*ft_create_stack(void);
+void				ft_SA_SB(t_stack *a);
+void				ft_SS(t_stack *a, t_stack *b);
+void				ft_PA_PB(t_stack *from, t_stack *to);
+void				ft_free_stack(t_stack *stack);
+
+/*
+**	-----------------\
+**	functions program
+**	-----------------/
+*/
+
+char				**move_list(t_node *node);
+void				apply_rule(char *move, t_stack *a, t_stack *b);
+int					assaign_weight(t_node *node);
 
 #endif
