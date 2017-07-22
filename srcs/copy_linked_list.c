6fde20b9f36e12a67d5e7c2cac35252a3c3d5a76
Jerome-Damhuis <Jerome-Damhuis@users.noreply.github.com>
2017-07-22 17:45:48 +0200

    done most of a star, changed bzero to make it clear you put '\0' in every space

--- srcs/copy_linked_list.c
+++ srcs/copy_linked_list.c
@@ -2,26 +2,14 @@
 
 // this is done wrong, you get t_stack not t_list will need to sort
 
-t_list		*copy_linked_list(t_list *list)
+t_stack		*copy_stack(t_stack *stack, )
 {
-	t_list	*new_list;
-	t_list	*current;
+	t_stack	*new_stack;
+	t_stack	*current;
 
-	if (list == NULL)
+	if (stack == NULL)
 		return NULL;
-	new_list = (t_list *)malloc(sizeof(t_list));
-	new_list->value = list->value;
-	new_list->prev = NULL;
-	current = new_list;
-	list = list->next;
-	while(list != NULL) 
-	{
-		current->next = malloc(sizeof(t_list));
-		(current->next)->prev = current;
-		current = current->next;
-		current->value = list->value;
-		list = list->next;
-	}
-	current->next = NULL;
+	new_stack = (t_stack *)malloc(sizeof(t_stack));
+	new_stack->front = ft_create_stack(pack->int_arr, pack->elements)
 	return (new_list);
 }
